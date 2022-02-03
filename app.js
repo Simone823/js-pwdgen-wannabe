a354b5e0fac45bbc4e19a9a7b26dc52817a4312a
Simone823 <simone.daglio823@outlook.it>
2022-02-03 17:13:25 +0100

    User surname e stampa userSurname in h1

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // User Name
-let userName = prompt ("Ehi come ti chiami? (Digita qui il tuo nome)")
+let userName = prompt ("Ehi come ti chiami? (Digita qui il tuo Nome)")
 console.log(userName)   
 
+// Stampa userName in index.html
 document.getElementById('userName').innerHTML = userName
 console.log(document)
 
+
+// User Surname
+let userSurname = prompt ("E qual è il tuo Cognome? (Digita qui il tuo Cognome)")
+console.log(userSurname)
+
+// Stampa userSurname in index.html
+document.getElementById("userSurname").innerHTML = userSurname
+console.log(document)
 
