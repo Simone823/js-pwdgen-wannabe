24091bbc3608ea977622e91b8c0f3dd96ff7879f
Simone823 <simone.daglio823@outlook.it>
2022-02-03 16:59:31 +0100

    userName prompt e  stampa userName h1

---
+++ app.js
@@ -0,0 +1,8 @@
+// User Name
+let userName = prompt ("Ehi come ti chiami? (Digita qui il tuo nome)")
+console.log(userName)   
+
+document.getElementById('userName').innerHTML = userName
+console.log(document)
+
+
